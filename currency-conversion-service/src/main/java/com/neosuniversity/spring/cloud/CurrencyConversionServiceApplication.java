8c0a7b59fdb3df0643b0119fca64c45e5250878a
hhugohm <hhugohm@icloud.com>
2018-12-20 13:22:21 -0600

    @FeignClient  and @RibbonClient on boot

--- currency-conversion-service/src/main/java/com/neosuniversity/spring/cloud/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/neosuniversity/spring/cloud/CurrencyConversionServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.neosuniversity.spring.cloud")
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
